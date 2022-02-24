2e0a7c38638d5a25c9032d1974398453c4ae703d
ZxyKira <danny41553@gmail.com>
2022-02-25 02:53:41 +0800

    [add] adc feture

---
+++ src/main/core/arterytek/at32f415/analog/input/package-info.h
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2020 ZxyKira
+ * All rights reserved.
+ * 
+ * SPDX-License-Identifier: MIT
+ */
+#ifndef CORE_ARTERYTEK_AT32F415_42D8E517_ABD8_41F1_95DC_6BB39EC50E85
+#define CORE_ARTERYTEK_AT32F415_42D8E517_ABD8_41F1_95DC_6BB39EC50E85
+
+/* ****************************************************************************************
+ * Include folder
+ */
+
+/* ****************************************************************************************
+ * Include
+ */
+#include "CoreAnalogInputPort.h"
+
+/* ****************************************************************************************
+ * End of file
+ */
+
+#endif /* CORE_ARTERYTEK_AT32F415_42D8E517_ABD8_41F1_95DC_6BB39EC50E85 */
