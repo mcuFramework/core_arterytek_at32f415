e00b85b63aea6b63b5ef5363d8e39c14955332fe
ZxyKira <danny41553@gmail.com>
2022-05-04 01:58:01 +0800

    [add] impotr bsp_arterytek_at32f415

---
+++ src/main/bsp_arterytek_at32f415/package-info.h
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2020 ZxyKira
+ * All rights reserved.
+ * 
+ * SPDX-License-Identifier: MIT
+ */
+#ifndef BSP_ARTERYTEK_AT32F415_B520CE77_2D48_4CFF_9511_78B5FB2AE5C7
+#define BSP_ARTERYTEK_AT32F415_B520CE77_2D48_4CFF_9511_78B5FB2AE5C7
+
+/* ****************************************************************************************
+ * Include folder
+ */
+
+/* ****************************************************************************************
+ * Include
+ */
+#include "at32f415.h"
+
+/* ****************************************************************************************
+ * End of file
+ */
+
+#endif/* BSP_ARTERYTEK_AT32F415_B520CE77_2D48_4CFF_9511_78B5FB2AE5C7 */
