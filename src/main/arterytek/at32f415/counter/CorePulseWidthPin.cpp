5dfabc0de9546afab89a990fe5d4e3172d27d7a6
ZxyKira <danny41553@gmail.com>
2022-05-25 11:23:38 +0800

    [add] CorePulseWidthPin

---
+++ src/main/arterytek/at32f415/counter/CorePulseWidthPin.cpp
@@ -0,0 +1,148 @@
+/**
+ * Copyright (c) 2020 ZxyKira
+ * All rights reserved.
+ * 
+ * SPDX-License-Identifier: MIT
+ */
+
+/* ****************************************************************************************
+ * Include
+ */
+
+//-----------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------
+#include "arterytek/at32f415/counter/CorePulseWidthPin.h"
+
+/* ****************************************************************************************
+ * Macro
+ */
+
+/* ****************************************************************************************
+ * Using
+ */
+
+//-----------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------
+using arterytek::at32f415::counter::CorePulseWidthPin;
+
+/* ****************************************************************************************
+ * Variable <Static>
+ */
+
+/* ****************************************************************************************
+ * Construct Method
+ */
+/**
+ * @brief Construct a new Core Pulse Width Pin object
+ * 
+ * @param corePulseWidthPort 
+ * @param channel 
+ */
+CorePulseWidthPin::CorePulseWidthPin(arterytek::at32f415::counter::CorePulseWidthPort& corePulseWidthPort, int channel) : 
+mCorePulseWidthPort(corePulseWidthPort){
+  this->mChannel = channel;
+  return;
+}
+/**
+ * @brief Destroy the Core Pulse Width Pin object
+ * 
+ */
+CorePulseWidthPin::~CorePulseWidthPin(void){
+  return;
+}
+/* ****************************************************************************************
+ * Operator Method
+ */
+
+/* ****************************************************************************************
+ * Public Method <Static>
+ */
+
+/* ****************************************************************************************
+ * Public Method <Override> - hal::counter::PulseWidthPin
+ */
+
+/**
+ * @brief Get the Period object
+ * 
+ * @return int 
+ */
+int CorePulseWidthPin::getPeriod(void){
+  return this->mCorePulseWidthPort.getPeriod();
+}
+
+/**
+ * @brief Get the Duty object
+ * 
+ * @return float 
+ */
+float CorePulseWidthPin::getDuty(void){
+  return this->mCorePulseWidthPort.getDuty(this->mChannel);
+}
+
+/**
+ * @brief Get the Frequence object
+ * 
+ * @return float 
+ */
+float CorePulseWidthPin::getFrequence(void){
+  return this->mCorePulseWidthPort.getFrequence();
+}
+
+/**
+ * @brief Set the Duty object
+ * 
+ * @param percent 0 ~ 1.0
+ * @return true 
+ * @return false 
+ */
+bool CorePulseWidthPin::setDuty(float percent){
+  return this->mCorePulseWidthPort.setDuty(this->mChannel, percent);
+}
+
+/**
+ * @brief Set the Duty object
+ * 
+ * @param duty 
+ * @return true 
+ * @return false 
+ */
+bool CorePulseWidthPin::setDuty(int duty){
+  return this->mCorePulseWidthPort.setDuty(this->mChannel, duty);
+}
+
+/**
+ * @brief 
+ * 
+ * @return true 
+ * @return false 
+ */
+bool CorePulseWidthPin::stop(void){
+  return this->mCorePulseWidthPort.setDuty(this->mChannel, 0);
+}
+
+/* ****************************************************************************************
+ * Public Method
+ */
+
+/* ****************************************************************************************
+ * Protected Method <Static>
+ */
+
+/* ****************************************************************************************
+ * Protected Method <Override>
+ */
+
+/* ****************************************************************************************
+ * Protected Method
+ */
+
+/* ****************************************************************************************
+ * Private Method
+ */
+
+/* ****************************************************************************************
+ * End of file
+ */
