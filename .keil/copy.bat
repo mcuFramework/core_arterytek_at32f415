37e0c3f7b4d5191c3ac766048d3434061e3f7cbb
ZxyKira <danny1191365@kitsu-studio.net>
2023-01-30 19:15:32 +0800

    [remove] java tool

--- .keil/copy.bat
+++
@@ -1,5 +0,0 @@
-cd %~dp0
-SET GenFolder=..\
-
-
-copy .\Objects\%1 ..\%1 /y
--- .keil/make-package.bat
+++
@@ -1,3 +0,0 @@
-;java -Dfile.encoding=UTF-8 -jar packet-tool.jar package ./../src/main/arterytek/ CORE_ARTERYTEK_AT32F415_
-
-pause
